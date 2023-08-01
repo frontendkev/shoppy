8eb618f5b2955c754adef33d730ffe5b1d4d58b1
frontendkev <kevfrontdev@gmail.com>
2023-08-01 12:26:30 +0100

    updating shoppy

--- src/Services/Axios/axios.js
+++ src/Services/Axios/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
     baseURL: 'https://shoppy-service.onrender.com',
+    //baseURL: 'http://localhost:4000',
     withCredentials: false,
     headers: {
         "Content-Type": "application/json"
